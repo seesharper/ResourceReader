a8a143c10f8326c09f8bbe12b848d53bfadb86d2
Bernhard Richter <bernhard.richter@gmail.com>
2019-04-02 11:09:16 +0200

    Added test for unknown resource

--- src/ResourceReader.Tests/ResourceReaderTests.cs
+++ src/ResourceReader.Tests/ResourceReaderTests.cs
@@ -22,10 +22,22 @@ namespace ResourceReader.Tests
             var content = resources.SampleResource;
             content.Should().Be("This is a sample resource");
         }
+
+        [Fact]
+        public void ShouldThrowMeanfullExceptionWhenResourceIsNotFound()
+        {
+            var resources = new ResourceBuilder().AddAssembly(typeof(ResourceReaderTests).Assembly).Build<IResources>();
+
+            Action act = () => {var content = resources.UnknownResource;};
+
+            act.Should().Throw<InvalidOperationException>().WithMessage("Unable to find any resources that matches 'UnknownResource'");
+        }
     }
 
     public interface IResources
     {
         string SampleResource { get; }
+
+        string UnknownResource { get; }
     }
 }
